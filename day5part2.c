2b29668330fd6d2bf8cffd8ab74a2c73de106c90
vaibhavsandhu <vaibhav.sandhu8@gmail.com>
2026-09-01 23:17:32 +0530

    Add files via upload

---
+++ day5part2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int seconds, hours, minutes, remainingSeconds;
+
+    scanf("%d", &seconds);
+
+    hours = seconds / 3600;
+    minutes = (seconds % 3600) / 60;
+    remainingSeconds = seconds % 60;
+
+    printf("%d:%d:%d", hours, minutes, remainingSeconds);
+
+    return 0;
+}
